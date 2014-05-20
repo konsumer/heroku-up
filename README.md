9a02fb7b702b1d056a0011bc79450e8441652417
David Konsumer <konsumer@jetboystudio.com>
2014-05-19 17:49:24 -0700

    should be complete, deploying

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
 # heroku-up
 
-Ping all your heroku apps, so they don't go down.
+Ping all your heroku apps, so they don't go to sleep.
 
-This is similar to [heroku-pinger](https://github.com/dcampano/heroku-pinger), but you don't have to set anything up, other than your token & deploying the code.
+This is similar to [heroku-pinger](https://github.com/dcampano/heroku-pinger), but you don't have to set anything up, other than your heroku token & deploying the code. If you add new web apps, it will ping them, automatically.
 
 ## setup
 
 Replace `my-heroku-pinger` with whatever you want your app to be called on heroku.
 
-The dyno will do all teh work. Here is how you first install it:
+The dyno will do all the work. Here is how you first install it:
 
 *  Fork/clone this repo
 *  `cd heroku-up`
 *  Create a heroku app to do the pinging: `heroku apps:create my-heroku-pinger -s cedar`
 *  Set your heroku API token (check [account page](https://dashboard.heroku.com/account), click "Show API Key"): `heroku config:add HEROKU_API_TOKEN="TOKEN_GOES_HERE"`
 *  Push yer code: `git push heroku master`
-*  Spin up a single dyno: `heroku ps:scale worker=1`
+*  Spin up a single worker dyno: `heroku ps:scale worker=1`
 
 ### blacklist
 
@@ -24,3 +24,7 @@ This is totally optional. Set the environment variable `SITE_BLACKLIST` to a com
 ```
 heroku config:add SITE_BLACKLIST="my-heroku-pinger,app1,app2,app3,another-heroku-project"
 ```
+
+### logs
+
+Like any heroku app, you can see how it's going by running `heroku logs`
--- dyno.js
+++ dyno.js
@@ -1,40 +1,43 @@
-var Heroku = require('heroku-client');
+var Heroku = require('heroku-client'),
+	url = require('url'),
+	http = require('http');
 
 if (!process.env.HEROKU_API_TOKEN){
-	console.log('Please check the README about setting up HEROKU_API_TOKEN.  This is not optional. Quitting.');
+	console.log('Fatal: Please check the README about setting up HEROKU_API_TOKEN. This is not optional. Quitting.');
 	process.exit(1);
 }
 
 if (!process.env.SITE_BLACKLIST){
-	console.log('Check the README for info about configuring the optional SITE_BLACKLIST, if you need that.');
+	console.log('Optional: Check the README for info about configuring the SITE_BLACKLIST, if you need that.');
+}
+
+function genericErr(err){
+	if (err) {
+		console.log(err);
+		process.exit(1);
+	}
 }
 
 function pingSites(){
+	setTimeout(pingSites, 3000000);
 	var blacklist = (process.env.SITE_BLACKLIST) ? process.env.SITE_BLACKLIST.split(',') : [];
 	var heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN });
 	heroku.apps().list(function (err, apps) {
-		if (err) {
-			console.log(err);
-			process.exit(1);
-		}
-		var urls = apps
-		.filter(function(a){
-			return apps.length && blacklist.indexOf(a.name) === -1;
-		})
-		.map(function(a){
-			return a.web_url;
-		});
-		console.log('pinging sites:\n  ' + urls.join('\n  '));
-
-		apps.forEach(function(a){
-			heroku.apps(a.name).info(function(err, app){
-				if (err) {
-					console.log(err);
-					process.exit(1);
-				}
-				console.log(app);
+		genericErr(err);
+		apps.forEach(function(app){
+			heroku.apps(app.name).dynos().list(function(err, dynos){
+				genericErr(err);
+				var urls = dynos.filter(function(dyno){
+					return dyno.type == 'web' && blacklist.indexOf(app.name) === -1;
+				}).forEach(function(dyno){
+					http.get(url.parse(app.web_url), function(res) {
+						console.log("Got response: ("+app.name+"): " + res.statusCode);
+					}).on('error', function(e) {
+						console.log("Got error: ("+app.name+"): " + e.message);
+					});
+				});
 			});
-		})
+		});
 	});
 	
 }
